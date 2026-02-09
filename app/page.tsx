7b02a8846416a93b4517675c6cfb44fff8fad249
lilKriT <lilKriTwebDev@gmail.com>
2026-02-09 01:06:47 +0100

    Lots of styling.

--- app/page.tsx
+++ app/page.tsx
@@ -9,37 +9,6 @@ export default function Home() {
       <Features />
       <p>About Services Projects Contact</p>
 
-      <p>Transform your brand</p>
-
-      <p>
-        We are a full-service creative agency specializing in helping brands
-        grow fast. Engage your clients through compelling visuals that do most
-        of the marketing for you.
-      </p>
-
-      <p>Learn more</p>
-
-      <p>Stand out to the right audience</p>
-
-      <p>
-        Using a collaborative formula of designers, researchers, photographers,
-        videographers, and copywriters, we’ll build and extend your brand in
-        digital places.{" "}
-      </p>
-
-      <p>Learn more</p>
-
-      <p>
-        Graphic design Great design makes you memorable. We deliver artwork that
-        underscores your brand message and captures potential clients’
-        attention.
-      </p>
-
-      <p>
-        Photography Increase your credibility by getting the most stunning,
-        high-quality photos that improve your business image.
-      </p>
-
       <p>Client testimonials</p>
 
       <p>
--- app/sections/Features.tsx
+++ app/sections/Features.tsx
@@ -1,15 +1,67 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Features = () => {
   return (
     <section>
-      <div>
+      <div className="grid grid-cols-2">
         {/* Egg */}
-        {/* <Image src={""}/> */}
+        <div className="bg-[url('/desktop/image-transform.jpg')] bg-cover bg-center order-2"></div>
+
         {/* Transform */}
+        <div className="flex justify-end items-center order-1 py-36 pr-24">
+          <div className="max-w-prose flex flex-col justify-center items-start gap-8 ml-16">
+            <h2 className="text-5xl font-black">Transform your brand</h2>
+
+            <p className="text-gray-400">
+              We are a full-service creative agency specializing in helping
+              brands grow fast. Engage your clients through compelling visuals
+              that do most of the marketing for you.
+            </p>
+
+            <Link
+              href={"/"}
+              className="text-xl font-serif px-2 after:h-2 after:w-[calc(100%+1rem)] after:bg-yellow-500 after:block after:relative after:-left-2 after:right-8 after:-top-3 after:-z-10 after:opacity-30 after:rounded-full"
+            >
+              Learn more
+            </Link>
+          </div>
+        </div>
+
         {/* Cup */}
-        {/* <Image /> */}
+        <div className="bg-[url('/desktop/image-stand-out.jpg')] bg-cover bg-center order-3">
+          Cup
+        </div>
+
         {/* Stand out */}
+        <div className="order-4">
+          <p>Stand out to the right audience</p>
+
+          <p>
+            Using a collaborative formula of designers, researchers,
+            photographers, videographers, and copywriters, we’ll build and
+            extend your brand in digital places.{" "}
+          </p>
+
+          <p>Learn more</p>
+        </div>
+
+        {/* Cherries / Graphic design */}
+        <div className="bg-[url('/desktop/image-graphic-design.jpg')] bg-cover bg-center order-5">
+          <p>
+            Graphic design Great design makes you memorable. We deliver artwork
+            that underscores your brand message and captures potential clients’
+            attention.
+          </p>
+        </div>
+
+        {/* Orange / Photography */}
+        <div className="bg-[url('/desktop/image-photography.jpg')] bg-cover bg-center order-6">
+          <p>
+            Photography Increase your credibility by getting the most stunning,
+            high-quality photos that improve your business image.
+          </p>
+        </div>
       </div>
     </section>
   );
