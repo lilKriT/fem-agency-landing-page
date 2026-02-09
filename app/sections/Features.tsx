b81b82bb0970b73bdb1cc07429d13da1f899e419
lilKriT <lilKriTwebDev@gmail.com>
2026-02-09 02:50:54 +0100

    More work.

--- app/sections/Features.tsx
+++ app/sections/Features.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 const Features = () => {
   return (
     <section>
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 auto-rows-[42rem]">
         {/* Egg */}
         <div className="bg-[url('/desktop/image-transform.jpg')] bg-cover bg-center order-2"></div>
 
         {/* Transform */}
-        <div className="flex justify-end items-center order-1 py-36 pr-24">
+        <div className="flex justify-end items-center py-36 pr-24 order-1">
           <div className="max-w-prose flex flex-col justify-center items-start gap-8 ml-16">
             <h2 className="text-5xl font-black">Transform your brand</h2>
 
@@ -21,7 +21,7 @@ const Features = () => {
 
             <Link
               href={"/"}
-              className="text-xl font-serif px-2 after:h-2 after:w-[calc(100%+1rem)] after:bg-yellow-500 after:block after:relative after:-left-2 after:right-8 after:-top-3 after:-z-10 after:opacity-30 after:rounded-full"
+              className="text-2xl font-serif px-2 after:h-2 after:w-[calc(100%+1rem)] after:bg-yellow-500 after:block after:relative after:-left-2 after:right-8 after:-top-3 after:-z-10 after:opacity-30 after:rounded-full"
             >
               Learn more
             </Link>
@@ -29,37 +29,46 @@ const Features = () => {
         </div>
 
         {/* Cup */}
-        <div className="bg-[url('/desktop/image-stand-out.jpg')] bg-cover bg-center order-3">
-          Cup
-        </div>
+        <div className="bg-[url('/desktop/image-stand-out.jpg')] bg-cover bg-center order-3"></div>
 
         {/* Stand out */}
-        <div className="order-4">
-          <p>Stand out to the right audience</p>
+        <div className="flex justify-start items-center py-36 pl-24 order-4">
+          <div className="max-w-prose flex flex-col justify-center items-start gap-8 mr-16">
+            <h2 className="text-5xl font-black">
+              Stand out to the right audience
+            </h2>
 
-          <p>
-            Using a collaborative formula of designers, researchers,
-            photographers, videographers, and copywriters, we’ll build and
-            extend your brand in digital places.{" "}
-          </p>
+            <p className="text-gray-400">
+              Using a collaborative formula of designers, researchers,
+              photographers, videographers, and copywriters, we’ll build and
+              extend your brand in digital places.
+            </p>
 
-          <p>Learn more</p>
+            <Link
+              href={"/"}
+              className="text-2xl font-serif px-2 after:h-2 after:w-[calc(100%+1rem)] after:bg-yellow-500 after:block after:relative after:-left-2 after:right-8 after:-top-3 after:-z-10 after:opacity-30 after:rounded-full"
+            >
+              Learn more
+            </Link>
+          </div>
         </div>
 
         {/* Cherries / Graphic design */}
-        <div className="bg-[url('/desktop/image-graphic-design.jpg')] bg-cover bg-center order-5">
-          <p>
-            Graphic design Great design makes you memorable. We deliver artwork
-            that underscores your brand message and captures potential clients’
+        <div className="bg-[url('/desktop/image-graphic-design.jpg')] bg-cover bg-top order-5 flex flex-col justify-end items-center gap-8 p-16">
+          <h2 className="text-4xl font-black text-green-800">Graphic design</h2>
+          <p className="text-green-800 text-center max-w-[50ch] text-balance">
+            Great design makes you memorable. We deliver artwork that
+            underscores your brand message and captures potential clients’
             attention.
           </p>
         </div>
 
         {/* Orange / Photography */}
-        <div className="bg-[url('/desktop/image-photography.jpg')] bg-cover bg-center order-6">
-          <p>
-            Photography Increase your credibility by getting the most stunning,
-            high-quality photos that improve your business image.
+        <div className="bg-[url('/desktop/image-photography.jpg')] bg-cover bg-top order-6 flex flex-col justify-end items-center gap-8 p-16">
+          <h2 className="text-4xl font-black text-blue-800">Photography</h2>
+          <p className="text-blue-800 text-center max-w-[50ch] text-balance">
+            Increase your credibility by getting the most stunning, high-quality
+            photos that improve your business image.
           </p>
         </div>
       </div>
--- app/sections/Testimonials.tsx
+++ app/sections/Testimonials.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section className="min-h-dvh flex justify-center items-center">
+      <div>
+        <h2>Client testimonials</h2>
+
+        <p>
+          We put our trust in Sunnyside and they delivered, making sure our
+          needs were met and deadlines were always hit. Emily R. Marketing
+          Director
+        </p>
+
+        <p>
+          Sunnyside’s enthusiasm coupled with their keen interest in our brand’s
+          success made it a satisfying and enjoyable experience. Thomas S. Chief
+          Operating Officer
+        </p>
+
+        <p>
+          Incredible end result! Our sales increased over 400% when we worked
+          with Sunnyside. Highly recommended! Jennie F. Business Owner
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
