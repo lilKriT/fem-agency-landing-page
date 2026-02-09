184d38cd75189d9e9b3bcc3effffb81e003b015a
lilKriT <lilKriTwebDev@gmail.com>
2026-02-09 16:16:37 +0100

    Gallery

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Testimonials from "./sections/Testimonials";
+import Gallery from "./sections/Gallery";
 
 export default function Home() {
   return (
     <div className="">
       {/* <Hero /> */}
       {/* <Features /> */}
-      <Testimonials />
+      {/* <Testimonials /> */}
+      <Gallery />
       <p>About Services Projects Contact</p>
 
       <p>About Services Projects Contact</p>
--- app/sections/Gallery.tsx
+++ app/sections/Gallery.tsx
@@ -1,5 +1,12 @@
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <section className="grid grid-cols-4 grid-rows-[37.5rem]">
+      <div className="bg-[url('/desktop/image-gallery-milkbottles.jpg')] bg-cover bg-no-repeat bg-bottom"></div>
+      <div className="bg-[url('/desktop/image-gallery-orange.jpg')] bg-cover bg-no-repeat bg-top"></div>
+      <div className="bg-[url('/desktop/image-gallery-cone.jpg')] bg-cover bg-no-repeat bg-bottom"></div>
+      <div className="bg-[url('/desktop/image-gallery-sugarcubes.jpg')] bg-cover bg-no-repeat bg-center"></div>
+    </section>
+  );
 };
 
 export default Gallery;
